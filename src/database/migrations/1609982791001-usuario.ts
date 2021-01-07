ad76b4ea6f514841b91f89c46f5ed0209085a58c
Anderson Pereira Rodrigues <anderson.carro.95@gmail.com>
2021-01-06 22:47:51 -0300

    Base de server NodeJs

---
+++ src/database/migrations/1609982791001-usuario.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class usuario1609982791001 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+import "./database/conection";
+
+const app = express();
+
+app.get("/", (req, res, next) => {
+  res.json({ message: "LOGIN -> olá server está online" });
+});
+app.listen(3000);
